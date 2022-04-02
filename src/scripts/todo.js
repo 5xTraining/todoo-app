82768ffb6ab65cfbab36704f2b8ba29cc6d03e5f
Eddie <eddie@5xcampus.com>
2022-04-02 18:15:27 +0800

    add SCSS support

--- src/scripts/todo.js
+++ src/scripts/todo.js
@@ -1,1 +1,12 @@
-console.log("hello!")
+const linkLogin = document.querySelector("#login")
+const linkSignUp = document.querySelector("#sign_up")
+
+linkLogin.addEventListener("click", (e) => {
+  e.preventDefault()
+  console.log("login")
+})
+
+linkSignUp.addEventListener("click", (e) => {
+  e.preventDefault()
+  console.log("sign up")
+})
